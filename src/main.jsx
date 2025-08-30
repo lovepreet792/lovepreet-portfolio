c0cc11e88d438cb4ef0a6100454d070177352ee8
lovepreet <mehralovepreet03@gmail.com>
2025-08-31 00:18:41 +0530

    add hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home.jsx'
 import Project from './pages/Project.jsx'
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/project/:slug', element: <Project /> },
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:slug" element={<Project />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 )
